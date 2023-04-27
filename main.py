2753c573dbeaf1a6022dcfb0a96cd45556266ade
Stasvdovin <vdovin.kontent@gmail.com>
2023-04-27 17:57:41 +0500

    Adding functionality text_load

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ text_generator = load()
 
 st.title("Text generation application")
 enter_text = st.text_input("Enter the text", value="")
-length_text = st.slider("Text length", 0, 50, 1)
-iteration_text = st.slider("How many iterations", 0, 5, 1)
+length_text = st.slider("Text length", 10, 100, 10)
+iteration_text = st.slider("How many iterations", 1, 3, 1)
 
 
 def text_load(enter_text):
-    return text_generator(
-        enter_text, max_length=length_text, num_return_sequences=iteration_text
-    )
+    if not enter_text:
+        return "Please enter some text."
+    elif str(enter_text).isdigit():
+        return "Please enter some text."
+    else:
+        return text_generator(
+            enter_text, max_length=length_text, num_return_sequences=iteration_text
+        )
 
 
 if st.button("Output"):
